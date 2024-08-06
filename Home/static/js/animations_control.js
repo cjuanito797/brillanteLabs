329d51e3cfcfb46e03682ef766c7efbc35694d5e
cjuanito797 <jlizarraga@unomaha.edu>
2024-08-06 13:38:40 -0500

    created animation control for home-page.

---
+++ Home/static/js/animations_control.js
@@ -0,0 +1,60 @@
+const testimonial = document.querySelector("#testimonial-rdc");
+const about_me_summary = document.querySelector("#about_me_summary");
+const aboue_me_image = document.querySelector("#about_me_image");
+const service_cards = document.querySelector("#service_cards");
+
+// Intersection observer for the about me summary.
+function about_me_func(entries) {
+
+    if (!entries[0].isIntersecting) {
+        return;
+    } else {
+        about_me_summary.classList.add('active')
+        about_me_image.classList.add('active');
+    }
+}
+
+const aboue_me_options = {
+    threshold: 1,
+}
+
+const about_me_obs = new IntersectionObserver(about_me_func, aboue_me_options);
+about_me_obs.observe(about_me_summary);
+
+
+// Intersection observer for the testimonial section.
+function testimonial_rdc_func(entries) {
+
+    if (!entries[0].isIntersecting) {
+        console.log("Object is not intersecting!");
+        return;
+    } else {
+        console.log("Object is intersecting!");
+        testimonial.classList.add('active');
+    }
+}
+
+const testimonial_options = {
+    threshold: 0.95,
+}
+
+const testimonial_rdc_obs = new IntersectionObserver(testimonial_rdc_func, testimonial_options);
+testimonial_rdc_obs.observe(testimonial);
+
+// Intersection observer for the skills cards section.
+function service_cards_func(entries) {
+
+    if (!entries[0].isIntersecting) {
+        console.log("Object is not intersecting");
+        return;
+    } else {
+        service_cards.classList.add('active');
+    }
+}
+
+const service_cards_options = {
+    threshold: 0.05,
+}
+
+const service_cards_obs = new IntersectionObserver(service_cards_func, service_cards_options);
+service_cards_obs.observe(service_cards);
--- Home/static/js/main.js
+++ Home/static/js/main.js
@@ -2,8 +2,7 @@ const nav = document.querySelector('nav');
 const contactButton = document.querySelector("#contact_me_button");
 const contactText = document.querySelector("#contact_us_text");
 const scrollDown = document.querySelector("#scroll-down");
-const about_me_summary = document.querySelector("#about_me_summary");
-const aboue_me_image = document.querySelector("#about_me_image");
+
 
 //intersection observer
 function navCallBack(entries) {
@@ -11,14 +10,13 @@ function navCallBack(entries) {
     contactButton.classList.toggle('active', !entries[0].isIntersecting);
     contactText.classList.toggle('active', !entries[0].isIntersecting);
     scrollDown.classList.toggle('disappear', !entries[0].isIntersecting);
-    about_me_summary.classList.toggle('active', !entries[0].isIntersecting);
-    about_me_image.classList.toggle('active', !entries[0].isIntersecting);
+
 }
 
 const navOptions = {
     threshold: .90,
 }
 
-const navObs = new IntersectionObserver(navCallBack, navOptions)
+const navObs = new IntersectionObserver(navCallBack, navOptions);
 
 navObs.observe(document.querySelector('#callToAction'));
